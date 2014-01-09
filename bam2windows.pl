3312e36f01becdd9427fe317796a5b9f2492303e
sberri <sberri@bffe50a8-24e6-5b60-2e4a-02f5e4b55db6>
2014-01-09 10:03:18 +0000

    just changing email and version

--- bam2windows.pl
+++ bam2windows.pl
@@ -5,7 +5,7 @@
 # to see usage
 #
 #
-# Copyright (C) 2010 Stefano Berri <s.berri@leeds.ac.uk> or <s.berri@gmail.com>
+# Copyright (C) 2010 Stefano Berri <sberri@illumina.com> or <s.berri@gmail.com>
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,8 +21,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # created 25/08/2010
-# last update 20/07/2012
-my $version = "0.3.8";
+# last update 09/01/2014
+my $version = "0.3.9";
 
 
 
